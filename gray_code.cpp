fb11ab84c46b68ef188d6a29fa8110f290b53c29
Soumojit Baidya <soumojitbaidya80@gmail.com>
2026-01-19 16:39:53 +0530

    Nice

---
+++ gray_code.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> grayCode(int n){
+        vector<int> ans;
+        int size=1<<n;
+        ans.push_back(0);
+        ans.push_back(1);
+        if(ans.size()==size)
+            return ans;
+        int m;
+        while(ans.size()<size){
+            m=ans.size();
+            for(int i=m-1;i>=0;i--){
+                ans.push_back(ans[i]|m);
+            }
+        }
+        return ans;
+    }
+    //utilized mirror property and a pattern of the mirror property here
+};
